c04769ed7551044b4e2b1be1ce08c1ae2872d0ef
Lucas Agustín Morales Romero <42081278+LucasAMoralesRomero@users.noreply.github.com>
2018-08-25 00:39:01 -0300

    Create practica-entorno-grafico(cuadro-de-dialogo).java

---
+++ practica-entorno-grafico(cuadro-de-dialogo).java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entorno.grafico;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lucas A. Morales Romero
+ */
+public class EntornoGrafico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Buenas noches");
+        JOptionPane.showMessageDialog(null, "MI nombre es Lucas Agustin");
+        JOptionPane.showMessageDialog(null, "Mi apellido es Morales Romero");
+        JOptionPane.showMessageDialog(null, "Mi profesion es Vendedor");
+        JOptionPane.showMessageDialog(null, "Estoy estudiando en el programa Codo a Codo");
+        System.exit(0);
+    }
+    
+}
